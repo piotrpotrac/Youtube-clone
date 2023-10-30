2d2c90af1571b540ac23872d7ba0112acd76e5e2
Piotr Potrac <piotrpotrac@gmail.com>
2023-10-30 21:46:04 +0100

    Creating  Youtube clone app

---
+++ src/components/Videos.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
+
+const Videos = ({ videos }) => {
+  console.log(videos);
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+          {item.id.videoId && <VideoCard video={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
